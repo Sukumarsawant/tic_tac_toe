b415a8bc31f369325643eefb37f927fa03116fe8
Dijkstra <sawantsukumar@gmail.com>
2025-02-07 00:00:04 +0530

    Add files via upload
    Initial upload

---
+++ 1.project/tictactoe/src/TicTacToe.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+//
+
+public class TicTacToe {
+    int boardLength = 600 ; 
+    int boardHeight  = 650  ; 
+    //additional 50 for text 
+
+    JFrame frame = new JFrame();
+    TicTacToe(){
+    frame.setVisible(true);
+    frame.setSize(boardLength, boardHeight);
+    }
+}
